3c4503480fdf60ad884a5ffe3ee54f8203b91f28
Pedro Barrera <pemamsn@gmail.com>
2026-04-12 13:15:40 +0200

    [PATCH] Updated cors

--- src/templates/default/config/cors.js
+++ src/templates/default/config/cors.js
@@ -1,1 +1,5 @@
-module.exports = { origin: '*' };
+module.exports = {
+  origin: process.env.TRUSTED_ORIGINS?.split(',') || ['http://localhost:5173'], // Specify allowed origins
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
+  credentials: true
+};
